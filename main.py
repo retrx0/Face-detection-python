0d45dff3cdef92cc3be8a7a98f0301cc6a8c6faf
abdulrahmaniliyasu <abdulrahmaniliyasu86@gmail.com>
2020-12-22 13:11:35 +0100

    Hello World

---
+++ main.py
@@ -0,0 +1,67 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import imutils
+from imutils.video import VideoStream
+import time
+
+video_path = None
+if video_path is None:
+    vs = VideoStream().start()
+else:
+    vs = cv2.VideoCapture(video_path)
+
+while True:
+    vid_frame = vs.read()
+    vid_frame = imutils.resize(vid_frame, width=800)
+    # convert to gray scale
+    vid_frame_gray = cv2.cvtColor(vid_frame, cv2.COLOR_BGR2GRAY)
+
+    cascade_face = cv2.CascadeClassifier(
+        'data/haarcascades/haarcascade_frontalface_default.xml')
+    cascade_face_alt = cv2.CascadeClassifier(
+        'data/haarcascades/haarcascade_frontalface_alt.xml')
+    cascade_face_alt2 = cv2.CascadeClassifier(
+        'data/haarcascades/haarcascade_frontalface_alt2.xml')
+    cascade_eyes = cv2.CascadeClassifier(
+        'data/haarcascades/haarcascade_eye.xml')
+
+    faces_rects = cascade_face.detectMultiScale(vid_frame_gray, scaleFactor=1.2, minNeighbors=5)
+    faces_rect_alt = cascade_face_alt.detectMultiScale(vid_frame_gray, scaleFactor=1.2, minNeighbors=5)
+    faces_circle_alt2 = cascade_face_alt.detectMultiScale(vid_frame_gray, scaleFactor=1.2, minNeighbors=5)
+    eye_circles = cascade_eyes.detectMultiScale(vid_frame_gray, scaleFactor=1.2, minNeighbors=5)
+
+    for (x, y, w, h) in faces_rects:
+        cv2.rectangle(vid_frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
+    for (x, y, w, h) in faces_rect_alt:
+        cv2.rectangle(vid_frame, (x, y), (x + w, y + h), (0, 0, 255), 1)
+    for (x, y, w, h) in faces_circle_alt2:
+        cv2.circle(vid_frame, (x + int(w / 2), y + int(h / 2)), 150, (0, 255, 0), 1)
+        cv2.line(vid_frame, (x + int(w / 2) - 30, y + int(h / 2) + 80), (x + int(w / 2) + 30, y + int(h / 2) + 80),
+                 (0, 255, 255), 1)
+    for (x, y, w, h) in eye_circles:
+        cv2.circle(vid_frame, (x + int(w / 2), y + int(h / 2)), 7, (0, 255, 255), 1)
+
+    cv2.imshow("Face Detector", vid_frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+
+
+def convertToRGB(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+def detect_faces(cascade, test_image, scaleFactor=1.1):
+    # create a copy of the image to prevent any changes to the original one.
+    image_copy = test_image.copy()
+
+    # convert the test image to gray scale as opencv face detector expects gray images
+    gray_image = cv2.cvtColor(image_copy, cv2.COLOR_BGR2GRAY)
+
+    # Applying the haar classifier to detect faces
+    faces_rect = cascade.detectMultiScale(gray_image, scaleFactor=scaleFactor, minNeighbors=5)
+
+    for (x, y, w, h) in faces_rect:
+        cv2.rectangle(image_copy, (x, y), (x + w, y + h), (0, 255, 0), 15)
+
+    return image_copy
